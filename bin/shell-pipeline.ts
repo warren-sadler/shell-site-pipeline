11286df5ad37b5e26290570336c50cfbdc7adca1
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-11-27 14:11:55 -0600

    fix: correct build-spec path again but this time its personal

--- bin/shell-pipeline.ts
+++ bin/shell-pipeline.ts
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "@aws-cdk/core";
-import { ShellPipelineStack } from "../lib/stacks/shell-pipeline-stack";
+import { ShellPipelineStack } from "../lib/shell-pipeline-stack";
 import { BillingStack } from "../lib/stacks/billing-stack";
 
 const app = new cdk.App();
