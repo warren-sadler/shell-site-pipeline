23328d921e5504d921055b11a3e93867b203a5fd
Warren Sadler <42621284+warren-sadler@users.noreply.github.com>
2021-11-27 14:30:14 -0600

    fix: update build spec

--- test/shell-pipeline.test.ts
+++ test/shell-pipeline.test.ts
@@ -1,7 +1,6 @@
-// import { Template } from '@aws-cdk/assertions';
 import { expect as expectCDK, haveResourceLike } from "@aws-cdk/assert";
 import * as cdk from "@aws-cdk/core";
-import { ShellPipelineStack } from "../lib/stacks/shell-pipeline-stack";
+import { ShellPipelineStack } from "../lib/shell-pipeline-stack";
 
 const app = new cdk.App();
 new ShellPipelineStack(app, "ShellPipeline");
